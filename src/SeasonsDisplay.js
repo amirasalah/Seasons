c755354bee22a5fcc2a4e5dc28f0588e756b490a
Amira Salah <amirasalah@Amiras-MacBook-Pro.local>
2018-12-16 21:59:52 +0200

    Detect users location

---
+++ src/SeasonsDisplay.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const SeasonsDisplay = () => {
+    return <div>Seasons</div>
+}
+export default SeasonsDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            latitude: '',
+            longitude: ''
+        }
+    }
+    render() {
+        navigator.geolocation.getCurrentPosition(
+            success => {
+                this.setState({
+                    latitude: success.coords.latitude,
+                    longitude: success.coords.longitude
+                })
+            },
+            error => {
+                console.log(error);
+            },
+        );
+        return (<div> <strong>Latitude:</strong>  {this.state.latitude}, <strong>Longitude:</strong>  {this.state.longitude} </div>);
+        }
+    
+    }
+ReactDOM.render( < App /> , document.getElementById("root"));
+serviceWorker.register();
