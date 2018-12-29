9cadd529da8e1bece6a228b3ecb517596f2ea00e
Amira Salah <amirasalah@Amiras-MacBook-Pro.local>
2018-12-29 19:34:23 +0200

    Updates

---
+++ precache-manifest.1176df9191d7f3882f6d7ece5ce8cf07.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d437c792baba5f999744",
+    "url": "/Seasons/static/js/runtime~main.d437c792.js"
+  },
+  {
+    "revision": "31e4f276fb0f8257f596",
+    "url": "/Seasons/static/js/main.31e4f276.chunk.js"
+  },
+  {
+    "revision": "2e45858fbe6fe8bc8cf2",
+    "url": "/Seasons/static/js/1.2e45858f.chunk.js"
+  },
+  {
+    "revision": "7c2c4ad8e642615300e7f5884e5f6c4d",
+    "url": "/Seasons/index.html"
+  }
+];
